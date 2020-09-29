1a17c819efed0c5a1cb3252210af061e7fb6f68f
Silas Vedder <svedder@buhl-data.com>
2020-09-29 13:44:48 +0200

    Clojure project initiated

---
+++ cah/doc/intro.md
@@ -0,0 +1,3 @@
+# Introduction to cah
+
+TODO: write [great documentation](http://jacobian.org/writing/what-to-write/)
---
+++ cah/test/cah/core_test.clj
@@ -0,0 +1,7 @@
+(ns cah.core-test
+  (:require [clojure.test :refer :all]
+            [cah.core :refer :all]))
+
+(deftest a-test
+  (testing "FIXME, I fail."
+    (is (= 0 1))))
